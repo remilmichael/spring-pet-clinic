ab6178f4f6bdfcd746bc748e9f45485d35bc92bf
Remil Michael <mail@remil.me>
2020-10-22 16:10:17 +0530

    Initial project add

---
+++ src/test/java/me/remil/springpetclinic/SpringPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package me.remil.springpetclinic;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
