6906318e1a3210e555b22b0007339ed3a3844a68
Remil Michael <mail@remil.me>
2020-10-25 20:30:58 +0530

    Added Owners List and updated controller. Closes #16

--- pet-clinic-web/src/main/java/me/remil/springpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/me/remil/springpetclinic/bootstrap/DataLoader.java
@@ -28,9 +28,9 @@ public class DataLoader implements CommandLineRunner {
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner1.setId(2L);
-        owner1.setFirstName("Fiona");
-        owner1.setLastName("Glenanne");
+        owner2.setId(2L);
+        owner2.setFirstName("Fiona");
+        owner2.setLastName("Glenanne");
         ownerService.save(owner2);
 
         Vet vet1 = new Vet();
