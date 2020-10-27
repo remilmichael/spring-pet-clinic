83c57dcf0366b5a7dbd38cd4b2d6c2cc160bcea1
Remil Michael <mail@remil.me>
2020-10-27 22:26:42 +0530

    added PetTypeService. closes #29

---
+++ pet-clinic-data/src/main/java/me/remil/springpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package me.remil.springpetclinic.services;
+
+import me.remil.springpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
---
+++ pet-clinic-data/src/main/java/me/remil/springpetclinic/services/map/PetTypeMapService.java
@@ -0,0 +1,33 @@
+package me.remil.springpetclinic.services.map;
+
+import me.remil.springpetclinic.model.PetType;
+import me.remil.springpetclinic.services.PetTypeService;
+
+import java.util.Set;
+
+public class PetTypeMapService extends AbstractMapService<PetType, Long> implements PetTypeService {
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
