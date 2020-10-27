54122757bab4ce81fc5f5a6781bd5801d121a1dc
Remil Michael <mail@remil.me>
2020-10-27 21:23:49 +0530

    added specialties, added to vet. closes #27

---
+++ pet-clinic-data/src/main/java/me/remil/springpetclinic/model/Speciality.java
@@ -0,0 +1,14 @@
+package me.remil.springpetclinic.model;
+
+public class Speciality extends BaseEntity{
+
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
