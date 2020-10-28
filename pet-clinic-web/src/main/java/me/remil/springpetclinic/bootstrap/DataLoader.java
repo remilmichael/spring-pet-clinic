63f659d528934298ed587dbd19222addc6f32271
Remil Michael <mail@remil.me>
2020-10-28 10:59:51 +0530

    Added Pets and contact info to owners on startup. closes #33

--- pet-clinic-web/src/main/java/me/remil/springpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/me/remil/springpetclinic/bootstrap/DataLoader.java
@@ -1,6 +1,7 @@
 package me.remil.springpetclinic.bootstrap;
 
 import me.remil.springpetclinic.model.Owner;
+import me.remil.springpetclinic.model.Pet;
 import me.remil.springpetclinic.model.PetType;
 import me.remil.springpetclinic.model.Vet;
 import me.remil.springpetclinic.services.OwnerService;
@@ -8,6 +9,8 @@ import me.remil.springpetclinic.services.PetTypeService;
 import me.remil.springpetclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
 
 @Component
 public class DataLoader implements CommandLineRunner {
@@ -33,16 +36,35 @@ public class DataLoader implements CommandLineRunner {
         cat.setName("Cat");
         PetType savedCatPetType = petTypeService.save(cat);
 
-
         Owner owner1 = new Owner();
         owner1.setFirstName("Michael");
         owner1.setLastName("Weston");
+        owner1.setAddress("123 Brickerel");
+        owner1.setCity("Miami");
+        owner1.setTelephone("12312344");
         ownerService.save(owner1);
+
+        Pet pet1 = new Pet();
+        pet1.setPetType(savedDogPetType);
+        pet1.setOwner(owner1);
+        pet1.setBirthDate(LocalDate.now());
+        pet1.setName("Rosco");
+        owner1.getPets().add(pet1);
 
         Owner owner2 = new Owner();
         owner2.setFirstName("Fiona");
         owner2.setLastName("Glenanne");
+        owner2.setAddress("456 Brickerel");
+        owner2.setCity("Miami");
+        owner2.setTelephone("12312344");
         ownerService.save(owner2);
+
+        Pet pet2 = new Pet();
+        pet2.setName("Meow");
+        pet2.setOwner(owner2);
+        pet2.setPetType(savedCatPetType);
+        pet2.setBirthDate(LocalDate.now());
+        owner2.getPets().add(pet2);
 
         Vet vet1 = new Vet();
         vet1.setFirstName("Sam");
