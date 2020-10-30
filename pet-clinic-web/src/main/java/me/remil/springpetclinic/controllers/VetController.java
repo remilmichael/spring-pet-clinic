e563bdb169ce166c0887ae5f877402dcae31a672
Remil Michael <mail@remil.me>
2020-10-30 12:34:07 +0530

    Updated base entity with JPA annotations. closes #35

--- pet-clinic-web/src/main/java/me/remil/springpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/me/remil/springpetclinic/controllers/VetController.java
@@ -15,7 +15,7 @@ public class VetController {
         this.vetService = vetService;
     }
 
-    @RequestMapping({"", "/", "/vets/index", "/vets/index.html"})
+    @RequestMapping({"", "/", "/index"})
     public String listVets(Model model) {
         model.addAttribute("vets", vetService.findAll());
         return "vets/index";
