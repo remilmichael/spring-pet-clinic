ce67929ef48a7f71bf8e1ef5db768d78cf2c0573
Remil Michael <mail@remil.me>
2020-10-31 09:58:54 +0530

    added Pet SD JPA Service. closes #43

---
+++ pet-clinic-data/src/main/java/me/remil/springpetclinic/services/springdatajpa/PetSDJpaService.java
@@ -0,0 +1,48 @@
+package me.remil.springpetclinic.services.springdatajpa;
+
+import me.remil.springpetclinic.model.Pet;
+import me.remil.springpetclinic.repositories.PetRepository;
+import me.remil.springpetclinic.services.PetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class PetSDJpaService implements PetService {
+
+    private final PetRepository petRepository;
+
+    public PetSDJpaService(PetRepository petRepository) {
+        this.petRepository = petRepository;
+    }
+
+    @Override
+    public Set<Pet> findAll() {
+        Set<Pet> petSet = new HashSet<>();
+        petRepository.findAll().forEach(petSet::add);
+        return petSet;
+    }
+
+    @Override
+    public Pet findById(Long aLong) {
+        return petRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return petRepository.save(object);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        petRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        petRepository.deleteById(aLong);
+    }
+}
